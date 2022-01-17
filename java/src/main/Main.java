049723539704f2114acc659fe7a2715506fa7850
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-17 13:24:49 -0300

    Update Main.java

--- java/src/main/Main.java
+++ java/src/main/Main.java
@@ -1,13 +1,16 @@
 package main;
 
 import adoroCinema.AdoroCinema;
+import app.frame.Frame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		AdoroCinema adoro = new AdoroCinema();
 		
-		
+		Frame frame = new Frame(adoro);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 
 	}
 
