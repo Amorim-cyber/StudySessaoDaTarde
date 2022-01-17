9ddf5c51ae15c9859f1b1d2b2d6d0f499ceba9fa
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-17 13:31:20 -0300

    Add Web.java

---
+++ java/src/web/Web.java
@@ -0,0 +1,157 @@
+package web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotInteractableException;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Web {
+
+	
+	public void digitarDados(ChromeDriver driver, String tipo,String tag, String dado) {
+		boolean b = false;
+		do {
+			//System.out.println("Digitando");
+			try {
+				b = false;
+				Thread.sleep(1000);
+				
+				switch(tipo) {
+					case "cssSelection":{
+						driver.findElement(By.cssSelector(tag)).sendKeys(dado);
+						break;
+					}
+					case "id":{
+						driver.findElement(By.id(tag)).sendKeys(dado);
+						break;
+					}
+					case "name":{
+						driver.findElement(By.name(tag)).sendKeys(dado);
+						break;
+					}
+					case "className":{
+						driver.findElement(By.className(tag)).sendKeys(dado);
+						break;
+					}
+					case "xpath":{
+						driver.findElement(By.xpath(tag)).sendKeys(dado);
+						break;
+					}
+				}
+				
+			} catch (NoSuchElementException | StaleElementReferenceException | ElementNotInteractableException e) {	
+				b = true;
+			} catch (InterruptedException e) {
+				b = true;
+			}
+		} while (b);
+	}
+	
+	public void clicar(ChromeDriver driver, String tipo,String tag) {
+		boolean b = false;
+		do {
+			System.out.println("Clique");
+			try {
+				b = false;
+				Thread.sleep(700);
+				
+				switch(tipo) {
+					case "cssSelection":{
+						driver.findElement(By.cssSelector(tag)).click();
+						break;
+					}
+					case "tag":{
+						driver.findElement(By.tagName(tag)).click();
+						break;
+					}
+					case "id":{
+						driver.findElement(By.id(tag)).click();
+						break;
+					}
+					case "name":{
+						driver.findElement(By.name(tag)).click();
+						break;
+					}
+					case "xpath":{
+						driver.findElement(By.xpath(tag)).click();
+						break;
+					}
+				}
+				
+			} catch (NoSuchElementException | InterruptedException | StaleElementReferenceException | ElementNotInteractableException e) {
+				b = true;
+			}
+		} while (b);
+	}
+	
+	public void enter(ChromeDriver driver,String tipo, String tag) {
+		boolean loop = false;
+		do {
+			//System.out.println("Enter");
+			try {
+				loop = false;
+				Thread.sleep(700);
+				
+				switch(tipo) {
+					case "cssSelection":{
+						driver.findElement(By.cssSelector(tag)).sendKeys(Keys.ENTER);
+						break;
+					}
+					case "id":{
+						driver.findElement(By.id(tag)).sendKeys(Keys.ENTER);
+						break;
+					}
+					case "name":{
+						driver.findElement(By.name(tag)).sendKeys(Keys.ENTER);
+						break;
+					}
+					case "xpath":{
+						driver.findElement(By.xpath(tag)).sendKeys(Keys.ENTER);
+						break;
+					}
+				}
+				
+			} catch (NoSuchElementException | InterruptedException e) {
+				loop = true;
+			}
+		} while (loop);
+	}
+	
+	public String pegarTxt(ChromeDriver driver, String tipo,String tag) {
+		boolean b = false;
+		String txt = "";
+		do {
+			System.out.println("Pegar txt");
+			try {
+				b = false;
+				Thread.sleep(800);
+				
+				switch(tipo) {
+					case "cssSelection":{
+						txt = driver.findElement(By.cssSelector(tag)).getText();
+						break;
+					}
+					case "id":{
+						txt = driver.findElement(By.id(tag)).getText();
+						break;
+					}
+					case "name":{
+						txt = driver.findElement(By.name(tag)).getText();
+						break;
+					}
+					case "xpath":{
+						txt = driver.findElement(By.xpath(tag)).getText();
+						break;
+					}
+				}
+				
+			} catch (NoSuchElementException | InterruptedException | StaleElementReferenceException | ElementNotInteractableException e) {
+				b = true;
+			}
+		} while (b);
+		return txt;
+	}
+	
+}
