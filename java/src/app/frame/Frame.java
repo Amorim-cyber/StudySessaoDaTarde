12b19ace2c3b4132b04979e02577c684ac7f88a7
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-17 13:21:47 -0300

    Update Frame.java

--- java/src/app/frame/Frame.java
+++ java/src/app/frame/Frame.java
@@ -2,18 +2,24 @@ package app.frame;
 
 import javax.swing.JFrame;
 
+import adoroCinema.AdoroCinema;
+import app.menuBar.Action;
+import app.menuBar.Bar;
 
 public class Frame extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 	private BackGround back = new BackGround();
 	
-	public Frame() {
+	public Frame(AdoroCinema adoro) {
+		
+		JFrame frame = this;
 		
 		setTitle("CinemaExtractor");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(0, 0, 760, 760);
 		setResizable(false);
+		setJMenuBar(new Bar(new Action(adoro,frame)));
 		setContentPane(back);
 		
 	}
