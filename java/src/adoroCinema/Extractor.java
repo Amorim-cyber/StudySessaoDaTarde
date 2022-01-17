e2349d8a2520712dd67f51c7ce4eb8fd8878fa0e
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-17 15:15:03 -0300

    Update Extractor.java

--- java/src/adoroCinema/Extractor.java
+++ java/src/adoroCinema/Extractor.java
@@ -12,6 +12,17 @@ public class Extractor extends Web{
 	private String skipButton = "/html/body/div[1]/div[1]/div[1]/a[2]";
 	private String movieTitle = "/html/body/div[1]/main/div[3]/div/section[1]/ul/li/div/div[1]/h2/a";
 	
+	private String release;
+	private String duration;
+	private String type;
+	private String director;
+	private String screenPlay;
+	private String casting;
+	private String originalTitle;
+	private String adoroRating;
+	
+	private String txt = "";
+	
 	public Extractor(ChromeDriver driver) {
 		this.driver = driver;
 	}
@@ -25,8 +36,31 @@ public class Extractor extends Web{
 			clicar(driver,"xpath", skipButton);
 			clicar(driver,"xpath", movieTitle);
 			
+			String info = pegarTxt(driver, "name","meta-body-item meta-body-info");
+			
+			release = info.split(" / ")[0];
+			duration = info.split(" / ")[1];
+			type = info.split(" / ")[2];
+			
+			director = pegarTxt(driver, "name","meta-body-item meta-body-direction");
+			screenPlay = pegarTxt(driver, "name","meta-body-item meta-body-direction");
+			casting = pegarTxt(driver, "name","meta-body-item meta-body-actor");
+			originalTitle = pegarTxt(driver, "name","meta-body-item");
+			adoroRating = pegarTxt(driver, "name","stareval-note"); 
+			
+			txt += movies[i] + "\t" 
+			+ release + "\t" 
+			+ duration + "\t" 
+			+ type + "\t" 
+			+ director + "\t" 
+			+ screenPlay + "\t" 
+			+ casting + "\t" 
+			+ originalTitle + "\t" 
+			+ adoroRating + "\n"; 
 		}
 		
+		System.out.println(txt);
+			
 	}
 
 }
