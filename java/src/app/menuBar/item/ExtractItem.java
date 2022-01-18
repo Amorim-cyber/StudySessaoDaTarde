c3fde62969d062b598b49d2faa4482c73dc13fcb
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-18 11:03:29 -0300

    Update ExtractItem.java

--- java/src/app/menuBar/item/ExtractItem.java
+++ java/src/app/menuBar/item/ExtractItem.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 
 import adoroCinema.AdoroCinema;
+import app.log.movies.Achieve;
 
 public class ExtractItem extends JMenuItem{
 
@@ -16,13 +17,16 @@ public class ExtractItem extends JMenuItem{
 		this.adoro = adoro; this.frame = frame;
 		setText("Extract films");
 		addActionListener(e -> extract());
-		
+		if(new Achieve().exist())
+			setEnabled(false);
 		
 	}
 	
 	public void extract() {
 		frame.setVisible(false);
 		adoro.extract();
+		if(new Achieve().exist())
+			setEnabled(false);
 		frame.setVisible(true);
 	}
 	
