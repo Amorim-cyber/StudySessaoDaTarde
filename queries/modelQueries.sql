3728b4b8499aca8330d987fe051b8f1461903f03
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-18 13:49:45 -0300

    Update modelQueries.sql

--- queries/modelQueries.sql
+++ queries/modelQueries.sql
@@ -35,3 +35,16 @@ as
 	insert into MODEL.T_Filmes values 
 	(@NomeFilme,@Estreia,@Duracao,@Categoria,@Diretor,@Roteiro,@Elenco,@NomeOriginal,@Nota)
 go
+
+/* realizando inserts em MODEL.T_Filmes */
+
+inserirModel 'Força de Viver', '5 de novembro de 2015 diretamente para TV', '1h 31min', 'Drama, Esporte', 'Sean McNamara', 'Howard Klausner', 'Cody Linley, Patrick Warburton, Lauren Holly', 'Hoovey', 3.7
+go
+inserirModel 'O Casamento Dos Meus Sonhos', '30 de março de 2001 No cinema', '1h 43min', 'Romance, Comédia', 'Adam Shankman', 'null', 'Jennifer Lopez, Matthew McConaughey, Bridgette Wilson', 'The Wedding Planner', 3.5
+go
+inserirModel 'O Amor De Um Pai', '2011', '1h 45min', 'Drama, Família', 'Michael M. Scott', 'null', 'Drew Seeley, Brittney Irvin, Ryan McDonald', 'Freshman Father', null
+go
+
+
+select * from MODEL.T_Filmes
+go
