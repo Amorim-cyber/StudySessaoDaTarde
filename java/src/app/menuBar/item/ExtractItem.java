cfe2ea895d526e3c976356b222226eab203a4515
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-21 19:50:40 -0300

    Update java/src/app/menuBar/item/ExtractItem.java

--- java/src/app/menuBar/item/ExtractItem.java
+++ java/src/app/menuBar/item/ExtractItem.java
@@ -3,18 +3,18 @@ package app.menuBar.item;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
 
-import adoroCinema.AdoroCinema;
 import app.log.movies.Achieve;
+import google.Google;
 
 public class ExtractItem extends JMenuItem{
 
 	private static final long serialVersionUID = 1L;
 	
-	private AdoroCinema adoro;
+	private Google google;
 	private JFrame frame;
 	
-	public ExtractItem(AdoroCinema adoro,JFrame frame) {
-		this.adoro = adoro; this.frame = frame;
+	public ExtractItem(Google google,JFrame frame) {
+		this.google = google; this.frame = frame;
 		setText("Extract films");
 		addActionListener(e -> extract());
 		if(new Achieve().exist())
@@ -24,7 +24,7 @@ public class ExtractItem extends JMenuItem{
 	
 	public void extract() {
 		frame.setVisible(false);
-		adoro.extract();
+		google.extract();
 		if(new Achieve().exist())
 			setEnabled(false);
 		frame.setVisible(true);
