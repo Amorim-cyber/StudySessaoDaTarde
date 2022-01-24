be7d790ccfe6f46f026b757be6a1b6501a68c986
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-01-24 19:57:43 -0300

    Add java/src/google/Movie.java

---
+++ java/src/google/Movie.java
@@ -0,0 +1,45 @@
+package google;
+
+public class Movie {
+
+	private Long id;
+	private String name;
+	private String time;
+	private String release;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public String getRelease() {
+		return release;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public void setRelease(String release) {
+		this.release = release;
+	}
+	
+	
+	
+	
+}
