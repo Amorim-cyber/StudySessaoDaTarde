d6cd3bdd26c63f623b2c70d7af4e78660b32dc9b
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-02-01 20:00:51 -0300

    Add java/src/google/RegisterScreenPlay.java

---
+++ java/src/google/RegisterScreenPlay.java
@@ -0,0 +1,23 @@
+package google;
+
+public class RegisterScreenPlay {
+	
+	private Long idMovie;
+	private Long idScreenPlay;
+	
+	public RegisterScreenPlay(Long idMovie, Long idScreenPlay) {
+		this.idMovie = idMovie;
+		this.idScreenPlay = idScreenPlay;
+	}
+
+	public Long getIdMovie() {
+		return idMovie;
+	}
+
+	public Long getIdScreenPlay() {
+		return idScreenPlay;
+	}
+	
+	
+
+}
