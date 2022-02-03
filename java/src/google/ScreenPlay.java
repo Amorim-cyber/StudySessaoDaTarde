36902353ae3943607dd7ebfa1a3fc103e2dc1fb5
Lucas Mattos Amorim <amorim22lucas@gmail.com>
2022-02-03 19:05:02 -0300

    Add java/src/google/ScreenPlay.java

---
+++ java/src/google/ScreenPlay.java
@@ -0,0 +1,21 @@
+package google;
+
+public class ScreenPlay {
+	
+	private Long id;
+	private String name;
+	
+	public ScreenPlay(Long id,String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
